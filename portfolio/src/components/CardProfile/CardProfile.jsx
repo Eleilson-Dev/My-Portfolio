923d7b6032f0693a21e597c574bb76b9c9cf6404
Elleylson Santtos <maxta.gamer@gmail.com>
2023-03-28 16:09:20 -0300

    update

--- portfolio/src/components/CardProfile/CardProfile.jsx
+++ portfolio/src/components/CardProfile/CardProfile.jsx
@@ -1,6 +1,6 @@
-import CardContentComponet from './CardContentComponet/CardContentComponet';
-import ProfileComponent from './ProfileComponent/ProfileComponent';
-import ProfileNavComponent from './ProfileNavComponent/ProfileNavComponent';
+// import CardContentComponet from './CardContentComponet/CardContentComponet';
+// import ProfileComponent from './ProfileComponent/ProfileComponent';
+// import ProfileNavComponent from './ProfileNavComponent/ProfileNavComponent';
 
 import './CardProfile.css';
 
@@ -8,11 +8,11 @@ const CardProfile = () => {
   return (
     <div className="card_profile">
       <div className="card_profile_top">
-        <ProfileComponent />
-        <ProfileNavComponent />
+        {/* <ProfileComponent />
+        <ProfileNavComponent /> */}
       </div>
       <div className="card_profile_bottom">
-        <CardContentComponet />
+        {/* <CardContentComponet /> */}
       </div>
     </div>
   );
